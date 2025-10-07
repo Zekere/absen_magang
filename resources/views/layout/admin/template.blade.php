0902750b939e9b711dd642dca3a99d3a4bba1c25
unknown <dityaka8@gmail.com>
2025-10-07 09:43:12 +0700

    Logout baru

--- resources/views/layout/admin/template.blade.php
+++ resources/views/layout/admin/template.blade.php
@@ -394,7 +394,6 @@
                         <a class="dropdown-item" href="#">Account Setting</a>
                         <div class="dropdown-divider"></div>             
                         <a class="dropdown-item" href="/logoutadmin" id="logout">Logout</a>
-                        <a class="dropdown-item" href="/logoutadmin" id="log">Logout</a>
                       </li>
                     </div>
                   </ul>
